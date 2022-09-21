0daafb09424dda94ee5ae80314b3b0b4989bd717
Alvaro Huertas <alvarohuertasgutierrez@gmail.com>
2022-09-20 22:39:29 -0500

    addJS

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const mostrarVideos = (videos) => {
+    const contenedorVideos = document.getElementById("contenedor_video")
+
+    videos.forEach(video => {
+        const div = document.createElement("div")
+        div.classList.add("row")
+        div.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-4 pt-3 pb-3">
+                            <article class="card-video effect">
+                            <iframe src="${video.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <div class="card-body">
+                                <h5 class="card-title fw-bold">${video.title}</h5>
+                                <p class="card-text my-4">${video.text}</p>
+                                </div>
+                            </article>
+                        </div>`
+
+        contenedorVideos.appendChild(div)
+    })
+}
+
+mostrarVideos(videos)
---
+++ js/stockVideos.js
@@ -0,0 +1,39 @@
+/* Array que contiene el listado de videos a publicar*/
+const videos = [
+    {
+		id: 1,
+		link: 'https://www.youtube.com/embed/AA6JcLBk6g8',
+		title: '1er Valida Trepadores Filandia 2022',
+		text: 'Primera valida de trepadores en el municipio de Filandia Quindio, conto con la participación de 60 pilotos de diferentes lugares del País, donde  se destacan los departamentos de Caldas, Quindio, Valle, Risaralda y Tolima.',
+	},
+    {
+		id: 2,
+		link: 'https://www.youtube.com/embed/AA6JcLBk6g8',
+		title: '2da Valida Trepadores Génova 2022',
+		text: 'Primera valida de trepadores en el municipio de Filandia Quindio, conto con la participación de 60 pilotos de diferentes lugares del País, donde  se destacan los departamentos de Caldas, Quindio, Valle, Risaralda y Tolima.',
+	},
+    {
+		id: 3,
+		link: 'https://www.youtube.com/embed/AA6JcLBk6g8',
+		title: '3er Valida Trepadores Pijao Q 2022',
+		text: 'Primera valida de trepadores en el municipio de Filandia Quindio, conto con la participación de 60 pilotos de diferentes lugares del País, donde  se destacan los departamentos de Caldas, Quindio, Valle, Risaralda y Tolima.',
+	},
+    {
+		id: 4,
+		link: 'https://www.youtube.com/embed/AA6JcLBk6g8',
+		title: '4ta Valida Trepadores Filandia 2022',
+		text: 'Primera valida de trepadores en el municipio de Filandia Quindio, conto con la participación de 60 pilotos de diferentes lugares del País, donde  se destacan los departamentos de Caldas, Quindio, Valle, Risaralda y Tolima.',
+	},
+    {
+		id: 5,
+		link: 'https://www.youtube.com/embed/AA6JcLBk6g8',
+		title: '5ta Valida Trepadores Filandia 2022',
+		text: 'Primera valida de trepadores en el municipio de Filandia Quindio, conto con la participación de 60 pilotos de diferentes lugares del País, donde  se destacan los departamentos de Caldas, Quindio, Valle, Risaralda y Tolima.',
+	},
+    {
+		id: 6,
+		link: 'https://www.youtube.com/embed/AA6JcLBk6g8',
+		title: '6ta Valida Trepadores Filandia 2022',
+		text: 'Primera valida de trepadores en el municipio de Filandia Quindio, conto con la participación de 60 pilotos de diferentes lugares del País, donde  se destacan los departamentos de Caldas, Quindio, Valle, Risaralda y Tolima.',
+	},
+]
